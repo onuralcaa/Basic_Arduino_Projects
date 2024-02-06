81468f26613881d61b32b0674719e6256f1c056e
Onur ALACA <onuralcaa@gmail.com>
2024-02-06 22:53:00 +0300

    check continue

--- Ses_Kontrol/Sound_check_RGB.ino/Sound_check_RGB.ino.ino
+++ Ses_Kontrol/Sound_check_RGB.ino/Sound_check_RGB.ino.ino
@@ -1,18 +1,22 @@
 // C++ code
 //
 int sensor = A0;
-
+int led = 13;
 int durum;
 
 void setup()
 {
   Serial.begin(9600);
+  pinMode(sensor, INPUT);
+  pinMode(led, OUTPUT);
 }
 
 void loop()
 {
  
-  durum = analogRead,(sensor);
+  durum = analogRead(sensor);
+
+  
  
  
 
