4d7484eaf7a3d5d668abd2136efd359c3863d1b9
Onur ALACA <onuralcaa@gmail.com>
2024-02-08 13:26:32 +0300

    Mode 1 start

--- Ses_Kontrol/Sound_check_RGB.ino/Sound_check_RGB.ino.ino
+++ Ses_Kontrol/Sound_check_RGB.ino/Sound_check_RGB.ino.ino
@@ -62,7 +62,20 @@ void Mode0()
 
 void Mode1()
 {
+  // Her seferinde hue değeri 0.001 artırılıyor ve 1 olduğunda başa dönülüyor
+  h = h + 0.001;
+  if (h >= 1.0) {
+    h = 0;
+  }
+  // hue değeri r,g,b bileşenlerine çevriliyor
+  h2rgb(h, r, g, b);
 
+  // Ortak anot RGB LED
+  analogWrite(r_led, 255 - r);
+  analogWrite(g_led, 255 - g);
+  analogWrite(b_led, 255 - b);
+
+  delay(50);
 }
 
 void Mode2()
@@ -94,7 +107,7 @@ void Mode2()
 
   //Serial.println(color);
 
-  switch(color)
+  switch (color)
   {
     case 0://BEYAZ
       digitalWrite(r_led, HIGH);
@@ -137,5 +150,59 @@ void Mode2()
       digitalWrite(g_led, LOW);
       digitalWrite(b_led, HIGH);
       break;
+  }
+}
+
+void h2rgb(float H, int& R, int& G, int& B) {
+  int var_i;
+  float S=1, V=1, var_1, var_2, var_3, var_h, var_r, var_g, var_b;
+
+  if ( S == 0 )              
+  {
+    R = V * 255;
+    G = V * 255;
+    B = V * 255;
+  }
+  else
+  {
+    var_h = H * 6;
+    if ( var_h == 6 ) var_h = 0;      
+    var_i = int( var_h ) ;            
+    var_1 = V * ( 1 - S );
+    var_2 = V * ( 1 - S * ( var_h - var_i ) );
+    var_3 = V * ( 1 - S * ( 1 - ( var_h - var_i ) ) );
+    if ( var_i == 0 ) {
+      var_r = V     ;
+      var_g = var_3 ;
+      var_b = var_1 ;
+    }
+    else if ( var_i == 1 ) {
+      var_r = var_2 ;
+      var_g = V;
+      var_b = var_1 ;
+    }
+    else if ( var_i == 2 ) {
+      var_r = var_1 ;
+      var_g = V;
+      var_b = var_3 ;
+    }
+    else if ( var_i == 3 ) {
+      var_r = var_1 ;
+      var_g = var_2 ;
+      var_b = V;
+    }
+    else if ( var_i == 4 ) {
+      var_r = var_3 ;
+      var_g = var_1 ;
+      var_b = V;
+    }
+    else {
+    var_r = V;
+    var_g = var_1 ;
+    var_b = var_2 ;
+    }
+    R = (1-var_r) * 255;        
+    G = (1-var_g) * 255;
+    B = (1-var_b) * 255;
   }
 }
